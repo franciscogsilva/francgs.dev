6dafe1d4626d6f086d315fddbde9cab807116723
Francisco Gonzalez <javiergonzalezsilva@gmail.com>
2024-02-08 09:28:25 -0500

    Update to tailwind colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -6,6 +8,15 @@ export default {
     colors: {
       primary: "#00fff7",
       secondary: "#bd8afc",
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.slate,
+      green: colors.emerald,
+      purple: colors.violet,
+      yellow: colors.amber,
+      pink: colors.fuchsia,
     },
   },
   plugins: [],
