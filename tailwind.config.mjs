22a98a54c04aae01fc5f5deef230ef69095cd44c
Francisco Gonzalez <javiergonzalezsilva@gmail.com>
2024-02-07 23:41:34 -0500

    Update to primary color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,11 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    colors: {
+      primary: "#00a4fb",
+      // secondary: "#5fe5e0",
+      // ...
+    },
   },
   plugins: [],
 };
