347202462a0336f17942e15ec4437eb67a6c1173
Francisco Gonzalez <javiergonzalezsilva@gmail.com>
2025-11-22 18:20:05 -0500

    feat: Upgrade to Tailwind CSS v4, migrating configuration to global CSS, updating Astro integration, and adjusting background height.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   site: "https://francgs.dev",
 });
